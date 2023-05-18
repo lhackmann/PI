f1bec96928a36abec4b678180a30d5184bba6e9b
Leonardo Cardoso <leonardohcardoso15@gmail.com>
2023-05-17 21:01:44 -0300

    Alteração codigo Profissional Model

--- models/Profissional.js
+++ models/Profissional.js
@@ -10,7 +10,38 @@ export class Profissional {
     }
 }
 
-export const getAll = () => {
+export const update = (id, profissional) => {
+    const profissionalToUpdate = findByPk(id)
+    if(!profissionalToUpdate) {
+    return false
+    }
+
+    const index = dbProfisional.indexOf(profissionalToUpdate)
+    dbProfisional[index] = profissional
+    return true
+
+}
+
+export const destroy = (id) => {
+    const profissional = findByPk(id)
+    if(!profissional) {
+        return false
+    }
+    const index = dbProfisional.indexOf(profissional)
+    dbProfisional.splice(index, 1)
+    return true
+}
+
+export const findByPk = (id) => {
+    return dbProfisional.find(profissional => profissional.id === id)
+}
+
+export const create = (profissional) => {
+    ConstantSourceNode.id = dbProfisional.length + 1
+    dbProfisional.push(profissional)
+}
+
+export const findAll = () => {
     return dbProfisional
 }
 
