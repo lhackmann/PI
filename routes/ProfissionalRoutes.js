f6af7582d94ae8a379ffa98099107f35a2830346
Leonardo Cardoso <leonardohcardoso15@gmail.com>
2023-05-17 21:05:47 -0300

    Alteração codigo ProfissionalRoutes

--- routes/ProfissionalRoutes.js
+++ routes/ProfissionalRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import ProfissionalController from '../controllers/ProfissionalController.js'
 
-const ProfissionalRouter = express.Router()
+const ProfissionalRouter = express.Router();
 
-ProfissionalRouter.get('/',ProfissionalController.list)
-ProfissionalRouter.get('/:id',ProfissionalController.find)
+ProfissionalRouter.get('/',ProfissionalController.getProfissional)
+ProfissionalRouter.post('/',ProfissionalController.createProfissional)
+ProfissionalRouter.get('/:id',ProfissionalController.getProfissionalById)
+ProfissionalRouter.delete('/:id',ProfissionalController.destroyProfissional)
+ProfissionalRouter.put('/:id',ProfissionalController.updateProfissional)
 
 export default ProfissionalRouter
