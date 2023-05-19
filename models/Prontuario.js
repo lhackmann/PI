39b33e5282afad47890f3adcfe78165c86aa76dc
Laura.Nog <lauralrnogueira@gmail.com>
2023-05-19 18:27:01 -0300

    alteracao prontuario

--- models/Prontuario.js
+++ models/Prontuario.js
@@ -4,11 +4,42 @@ export class Prontuario {
         this.nome_pac = nome_pac       
     }
 }
+export const update = (id, Prontuario) => {
+    const prontuarioToUpdate = findByPk(id)
+    if(!prontuarioToUpdate) {
+    return false
+    }
+    
+    const index = dbProntuario.indexOf(prontuarioToUpdate)
+    dbProntuario[index] = prontuario
+    return true
 
-export const getAll = () => {
+    
+}
+
+export const destroy = (id) => {
+    const prontuario = findByPk(id)
+    if(!prontuario) {
+        return false
+    }
+    const index = dbProntuario.indexOf(prontuario)
+    dbProntuario.splice(index, 1)
+    return true
+}
+
+export const findByPk = (id) => {
+    return dbProntuario.find(prontuario => prontuario.id === id)
+}
+
+export const create = (prontuario) => {
+    prontuario.id = dbProntuario.length + 1
+    dbProntuario.push(prontuario)
+}
+
+export const findAll = () => {
     return dbProntuario
 }
 
-export const dbProntuario = [
-    new Prontuario(1,"Ana"),
+export const dbProfisional = [
+    new Profissional("01","Ana"),
     ]
