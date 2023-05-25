1fe19ffb1bac264458653e9419e42ff72fdf92fc
Leonardo Cardoso <leonardohcardoso15@gmail.com>
2023-05-25 20:24:36 -0300

    correção router paciente

--- routes/PacienteRoutes.js
+++ routes/PacienteRoutes.js
@@ -3,7 +3,7 @@ import PacienteController from '../controllers/PacienteController.js'
 
 const PacienteRouter = express.Router()
 
-Pacienterouter.get('/', PacienteController.getPaciente)
+Pacienterouter.get('/', PacienteController.list)
 Pacienterouter.post('/', PacienteController.createPaciente)
 Pacienterouter.get('/:id', PacienteController.getPacienteById)
 Pacienterouter.delete('/:id', PacienteController.destroyPaciente)
