2aabc3f3735dacac92b024c4dee91927704c8efe
Leonardo Cardoso <leonardohcardoso15@gmail.com>
2023-05-25 20:33:06 -0300

    alteração agenda.routes

--- routes/AgendaRoutes.js
+++ routes/AgendaRoutes.js
@@ -3,7 +3,7 @@ import AgendaController from '../controllers/AgendaController.js'
 
 const AgendaRouter = express.Router();
 
-AgendaRouter.get('/',AgendaController.getAgenda)
+AgendaRouter.get('/',AgendaController.list)
 AgendaRouter.post('/',AgendaController.createAgenda)
 AgendaRouter.get('/:id',AgendaController.getAgendaById)
 AgendaRouter.delete('/:id',AgendaController.destroyAgenda)
