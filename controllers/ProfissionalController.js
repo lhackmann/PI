2a2a085db4e1740fade5535045ce9d9038182cff
Leonardo Cardoso <leonardohcardoso15@gmail.com>
2023-06-03 17:50:40 -0300

    alteração prossional controller

--- controllers/ProfissionalController.js
+++ controllers/ProfissionalController.js
@@ -1,4 +1,87 @@
-import Profissional from "../models/Profissional.js"
+import Profissional from "../models/Profissional.js";
+
+class ProfissionalController {
+  static async list(req, res) {
+    const profissional = await Profissional.findAll();
+    res.json(profissional);
+  }
+
+  static async createProfissional(req, res) {
+    const { id, nome, cpf, rg, profissao, ccr, assinatura } = req.body;
+    if (!id || !nome || !cpf || !rg || !profissao || !ccr || !assinatura) {
+      res
+        .status(400)
+        .json({
+          error:
+            "ID, Nome, cpf, rg, profissão, ccr e assinatura são obrigatórios!!!",
+        });
+      return;
+    }
+    const createdProfissional = await Profissional.create({
+      id,
+      nome,
+      cpf,
+      rg,
+      profissao,
+      ccr,
+      assinatura,
+    });
+    res.status(201).json(createdProfissional);
+  }
+
+  static async getProfissionalById(req, res) {
+    const id = parseInt(req.params.id);
+    const profissional = await Profissional.findByPk(id);
+
+    if (!profissional) {
+      res.status(404).json({ error: "Profissional não encontrado!" });
+      return;
+    }
+    res.json(profissional);
+  }
+
+  static async destroyProfissional(req, res) {
+    const id = parseInt(req.params.id);
+    const profissional = await Profissional.findByPk(id);
+    if (!profissional) {
+      res.status(404).json({ error: "Profissional não encontrado!" });
+      return;
+    }
+    await Profissional.destroy({ where: { id: profissional.id } });
+    res.json({ message: "Profissional removido com sucesso!" });
+  }
+
+  static async updateProfissional(req, res) {
+    const id = parseInt(req.params.id);
+    const profissional = await Profissional.findByPk(id);
+    if (!profissional) {
+      res.status(404).json({ error: "Profissional não encontrado!" });
+      return;
+    }
+
+    const { nome, cpf, rg, profissao, ccr, assinatura } = req.body;
+    if (!nome || !cpf || !rg || !profissao || !ccr || !assinatura) {
+      res
+        .status(400)
+        .json({
+          error:
+            "Nome, cpf, rg, profissão, ccr e assinatura são obrigatórios!!!",
+        });
+      return;
+    }
+    const updatedProfissional = await Profissional.update(
+      { nome, cpf, rg, profissao, ccr, assinatura },
+      { where: { id: profissional.id } }
+    );
+    res.json(updatedProfissional);
+  }
+}
+
+export default ProfissionalController;
+
+
+
+/*import Profissional from "../models/Profissional.js"
 
 class ProfissionalController {
     static async list(req,res){
@@ -38,7 +121,7 @@ class ProfissionalController {
         await Profissional.destroy({where: {id: profissional.id}})
         res.json({ message: 'Profissional removido com sucesso!'})
     }
-    
+
     static async updateProfissional(req, res) {
         const id = parseInt(req.params.id)
         const profissional = await Profissional.findByPk(id)
@@ -48,7 +131,7 @@ class ProfissionalController {
         }
 
         const {nome, cpf, rg, profissao, ccr, assinatura } = req.body
-        if(!nome || !cpf || !rg || !profissao || !ccr || !assinatura) {
+        if(!id, !nome || !cpf || !rg || !profissao || !ccr || !assinatura) {
             res.status(400).json({ error: 'Nome, cpf, rg, profissão, ccr e assinatura são obrigatórios!!!'})
             return
     }
@@ -57,4 +140,4 @@ class ProfissionalController {
    }
 }
 
-export default ProfissionalController
+export default ProfissionalController*/
